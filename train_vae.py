6a677fe96cda356d5f64e724c55f1f7256519d2a
safak <safakk.bilici@gmail.com>
2021-05-28 14:12:35 +0300

    trainer

--- train_vae.py
+++ train_vae.py
@@ -9,7 +9,171 @@ from tensorboardX import SummaryWriter
 from torch.utils.data import DataLoader
 from collections import OrderedDict, defaultdict
 
-
-
 from models.variational_gru import VariationalGRU
 from utils.model_utils import to_var, idx2word
+from utils.data_utils import TextDataLoader
+
+def kl_anneal_function(anneal_function, step, k, x0):
+    if anneal_function == 'logistic':
+        return float(1/(1+np.exp(-k*(step-x0))))
+    elif anneal_function == 'linear':
+        return min(1, step/x0)
+
+def loss_fn(NLL, logp, target, length, mean, logv, anneal_function, step, k, x0):
+    target = target[:, :torch.max(length).item()].contiguous().view(-1)
+    logp = logp.view(-1, logp.size(2))
+    NLL_loss = NLL(logp, target)
+    KL_loss = -0.5 * torch.sum(1 + logv - mean.pow(2) - logv.exp())
+    KL_weight = kl_anneal_function(anneal_function, step, k, x0)
+    return NLL_loss, KL_loss, KL_weight
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_name', type=str, default='data')
+    parser.add_argument('--data_dir', type=str, default='data')
+    parser.add_argument('--create_data', action='store_true')
+    parser.add_argument('--max_sequence_length', type=int, default=60)
+    parser.add_argument('--min_occ', type=int, default=1)
+    parser.add_argument('--test_data', action='store_true')
+
+    parser.add_argument('-ep', '--epochs', type=int, default=10)
+    parser.add_argument('-bs', '--batch_size', type=int, default=32)
+    parser.add_argument('-lr', '--learning_rate', type=float, default=0.001)
+
+    parser.add_argument('-eb', '--embedding_size', type=int, default=300)
+    parser.add_argument('-hs', '--hidden_size', type=int, default=256)
+    parser.add_argument('-nl', '--num_layers', type=int, default=1)
+    parser.add_argument('-bi', '--bidirectional', action='store_false')
+    parser.add_argument('-ls', '--latent_size', type=int, default=16)
+    parser.add_argument('-wd', '--word_dropout', type=float, default=0.4)
+    parser.add_argument('-ed', '--embedding_dropout', type=float, default=0.5)
+
+    parser.add_argument('-af', '--anneal_function', type=str, default='logistic')
+    parser.add_argument('-k', '--k', type=float, default=0.0025)
+    parser.add_argument('-x0', '--x0', type=int, default=2500)
+
+    parser.add_argument('-v', '--print_every', type=int, default=1000)
+    parser.add_argument('-log', '--logdir', type=str, default='logs')
+    parser.add_argument('-bin', '--save_model_path', type=str, default='models')
+    parser.add_argument('-cuda', '--cuda', type=bool, default=True)
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if args.cuda == False and device.type=="cuda":
+        device = torch.device("cpu")
+    if args.cuda == True and device.type=="cpu":
+        print("Cannot detect cuda device. Automatically switching to cpu.")
+
+
+    splits = ['train', 'valid'] + (['test'] if args.test_data else [])
+
+    datasets = OrderedDict()
+
+    for split in splits:
+        datasets[split] = TextDataLoader(
+            data_name=args.data_name
+            data_dir=args.data_dir,
+            split=split,
+            create_data=args.create_data,
+            max_sequence_length=args.max_sequence_length,
+            min_occ=args.min_occ
+        )
+
+    params = dict(
+        vocab_size=datasets['train'].vocab_size,
+        sos_idx=datasets['train'].sos_idx,
+        eos_idx=datasets['train'].eos_idx,
+        pad_idx=datasets['train'].pad_idx,
+        unk_idx=datasets['train'].unk_idx,
+        max_sequence_length=args.max_sequence_length,
+        embedding_size=args.embedding_size,
+        hidden_size=args.hidden_size,
+        word_dropout=args.word_dropout,
+        embedding_dropout=args.embedding_dropout,
+        latent_size=args.latent_size,
+        num_layers=args.num_layers,
+        bidirectional=args.bidirectional
+    )
+
+    model = VariationalGRU(**params)
+    model = model.to(device)
+
+    save_model_path = os.path.join(args.save_model_path, ts)
+    os.makedirs(save_model_path)
+
+    with open(os.path.join(save_model_path, 'model_params.json'), 'w') as f:
+        json.dump(params, f, indent=4)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    NLL = torch.nn.NLLLoss(ignore_index=datasets['train'].pad_idx, reduction='sum')
+
+    if args.cuda == True and device.type=="cuda":
+        tensor = torch.cuda.FloatTensor
+    else:
+        tensor = torch.Tensor
+    step = 0
+
+    for epoch in range(args.epochs):
+        for split in splits:
+
+            data_loader = DataLoader(
+                dataset=datasets[split],
+                batch_size=args.batch_size,
+                shuffle=split=='train',
+                pin_memory=torch.cuda.is_available()
+            )
+
+            tracker = defaultdict(tensor)
+            if split == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            for iteration, batch in enumerate(data_loader):
+                batch_size = batch['input'].size(0)
+                for k, v in batch.items():
+                    if torch.is_tensor(v):
+                        batch[k] = to_var(v)
+
+                logp, mean, logv, z = model(batch['input'], batch['length'])
+                NLL_loss, KL_loss, KL_weight = loss_fn(NLL, logp, batch['target'],
+                                                       batch['length'], mean, logv,
+                                                       args.anneal_function, step,
+                                                       args.k, args.x0)
+                loss = (NLL_loss + KL_weight * KL_loss) / batch_size
+
+                if split == 'train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+                    step += 1
+
+                tracker['ELBO'] = torch.cat((tracker['ELBO'], loss.data.view(1, -1)), dim=0)
+
+                if iteration % args.print_every == 0 or iteration+1 == len(data_loader):
+                    print("%s Batch %04d/%i, Loss %9.4f, NLL-Loss %9.4f, KL-Loss %9.4f, KL-Weight %6.3f"
+                          % (split.upper(), iteration, len(data_loader)-1, loss.item(), NLL_loss.item()/batch_size,
+                          KL_loss.item()/batch_size, KL_weight))
+
+                if split == 'valid':
+                    if 'target_sents' not in tracker:
+                        tracker['target_sents'] = list()
+                    tracker['target_sents'] += idx2word(batch['target'].data, i2w=datasets['train'].get_i2w(),
+                                                        pad_idx=datasets['train'].pad_idx)
+                    tracker['z'] = torch.cat((tracker['z'], z.data), dim=0)
+
+            print("%s Epoch %02d/%i, Mean ELBO %9.4f" % (split.upper(), epoch, args.epochs, tracker['ELBO'].mean()))
+
+            if split == 'valid':
+                dump = {'target_sents': tracker['target_sents'], 'z': tracker['z'].tolist()}
+                if not os.path.exists(os.path.join('dumps', ts)):
+                    os.makedirs('dumps/'+ts)
+                with open(os.path.join('dumps/'+ts+'/valid_E%i.json' % epoch), 'w') as dump_file:
+                    json.dump(dump,dump_file)
+
+            if split == 'train':
+                checkpoint_path = os.path.join(save_model_path, "vae_epoch%i.pt" % epoch)
+                torch.save(model.state_dict(), checkpoint_path)
+                print("Model saved at %s" % checkpoint_path)
