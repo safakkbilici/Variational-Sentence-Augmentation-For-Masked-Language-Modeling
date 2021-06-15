e06171cd9ce465507af835d5692467a2fba45d46
safak <safakk.bilici@gmail.com>
2021-06-15 23:02:53 +0300

    argparse fixed

--- pretrain_bert.py
+++ pretrain_bert.py
@@ -16,11 +16,11 @@ if __name__ == "__main__":
 
     parser.add_argument('-t', '--tokenizer', type=str, default='models/tokenizer')
     parser.add_argument('-d', '--data', type=str, default='data/expanded_corpus.txt')
-    parser.add_argument('-bsiz', '--tokenizer', type=int, default=64)
     parser.add_argument('-mlm','--mlm_prob', type=float, default=0.15)
 
-    parser.add_argument('e', '--epochs', type=int)
+    parser.add_argument('-e', '--epochs', type=int)
     parser.add_argument('-co', '--checkpoint_output', type=str, default='models/bert_pretraining')
+    parser.add_argument('-bls', '--block_size', type=int, default=64)
     parser.add_argument('-bs', '--batch_size', type=int, default=64)
     parser.add_argument('-ls', '--log_step', type=int, default=1000)
 
@@ -29,6 +29,6 @@ if __name__ == "__main__":
     model = bert_model(args.vocab_size, args.max_position_embeddings, args.num_attention_heads,
                        args.num_hidden_layers, args.type_vocab_size, args.from_checkpoints, args.cuda)
 
-    dataset, data_collator = bert_dataset(args.tokenizer, args.file_path, args.block_size, args.mlm_prob)
+    dataset, data_collator = bert_dataset(args.tokenizer, args.data, args.block_size, args.mlm_prob)
 
     train_bert(model, args.epochs,data_collator, dataset,args.checkpoint_output, args.batch_size, args.log_step)
