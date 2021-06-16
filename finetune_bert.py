decc47c42039640a813914029a2163a7f1378adf
safak <safakk.bilici@gmail.com>
2021-06-16 15:07:09 +0300

    sequence classification added

--- finetune_bert.py
+++ finetune_bert.py
@@ -1,1 +1,73 @@
 import argparse
+import torch
+import numpy as np
+import pandas as pd
+
+from models.bert_sentiment import (
+    bert_sentiment_model,
+    train_sentiment_model,
+    get_data_loaders,
+    save_sentiment_model
+)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-dt', '--downstream_task', type=str)
+    parser.add_argument('-m', '--bert_model', type=str)
+    parser.add_argument('-d', '--dataset', type=str)
+    parser.add_argument('-b', '--batch_size', type=int, default=64)
+    parser.add_argument('-t', '--tokenizer', type=str)
+    parser.add_argument('-e', '--epochs', type=int, default=3)
+    parser.add_argument('-ev', '--evaluation', type=bool, default=True)
+    parser.add_argument('-sdm', '--save_downstream_model', type=bool, default=False)
+    parser.add_argument('-sdo', '--save_downstream_optimizer', type=bool, default=False)
+    parser.add_argument('-sdp', '--save_downstream_path', type=str)
+    parser.add_argument('-l', '--load_downstream_model', type=bool, default=False)
+    parser.add_argument('-lop', '--load_downstream_optimizer', type=bool, default=False)
+    parser.add_argument('-dc', '--downstream_checkpoints_path', type=str, default=None)
+    parser.add_argument('-c', '--cuda', type=bool, default=True)
+    args = parser.parse_args()
+
+    if args.downstream_task == "sequence classification":
+        df_train = pd.read_csv(f"{args.dataset}/train.csv")
+        df_test = pd.read_csv(f"{args.dataset}/test.csv")
+
+        train_loader, val_loader = get_data_loaders(
+            df_train = df_train,
+            df_test = df_test,
+            tokenizer = args.tokenizer,
+            batch_size = args.batch_size)
+
+        bert_sentiment_model, optimizer, scheduler, loss_fn = bert_sentiment_model(
+            pretrained = args.bert_model,
+            epochs = args.epochs,
+            cuda = args.cuda,
+            load_model = args.load_downstream_model,
+            load_optimizer= args.load_downstream_optimizer,
+            load_path = args.downstream_checkpoints_path
+        )
+
+        bert_sentiment_model, optimizer = train_sentiment_model(
+            model = bert_sentiment_model,
+            train_dataloader = train_loader,
+            val_dataloader = val_loader,
+            optimizer = optimizer,
+            loss_fn = loss_fn,
+            epochs = args.epochs,
+            evaluation = args.evaluation,
+            cuda = args.cuda
+        )
+
+        if args.save_downstream_model and args.save_downstream_optimizer:
+            save_sentiment_model(
+                model = bert_sentiment_model,
+                optimizer = optimizer,
+                path = args.save_downstream_path
+            )
+    
+        elif args.save_downstream_model and args.save_downstream_optimizer == False:
+            save_sentiment_model(
+                model = bert_sentiment_model,
+                path = args.save_downstream_path
+            )
+    
